118e412b9def9b33145e53e61c75e347b763c80f
verydecent <wnjhwng@gmail.com>
2020-06-07 23:28:07 -0700

    Create inner containers for nav

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <div className='nav'>
-      Navigation Component
+      <div className='nav__global-logo'>
+        <Link to='/'>Logo Here</Link>
+      </div>
+      <nav className='nav__menu'>
+        <Link to='/'>Home</Link>
+        <Link to='/concept'>Concept</Link>
+      </nav>
+      <nav className='nav__utility'>
+        <Link to='log-in'>Log In</Link>
+        <Link to='register'>Register</Link>
+      </nav>
     </div>
   );
 }
