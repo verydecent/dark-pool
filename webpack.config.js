5ddaf8a57acb86dc8fd4f6614c4a950c9c388fda
verydecent <wnjhwng@gmail.com>
2020-06-07 20:42:02 -0700

    Remove module option for css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,15 +12,7 @@ module.exports = {
       {
         test: /\.css$/i,
         exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
-        ]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
