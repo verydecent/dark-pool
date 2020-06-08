07c17c5b0a6c4cea4f8d15436373d8b8de6edb12
verydecent <wnjhwng@gmail.com>
2020-06-07 22:25:58 -0700

    import base css files to root

--- src/Root/index.js
+++ src/Root/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './global.css';
+import './font.css';
+import './reset.css';
 
 const Root = () => {
   return (
