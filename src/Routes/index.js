7311594a87ea4d30c79c2739d97fc3ef570030a8
verydecent <wnjhwng@gmail.com>
2020-06-08 00:41:36 -0700

    Replace fragment with switch component

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from '../Home';
 import Nav from '../Nav';
 
 const Routes = () => {
   return (
-    <>
+    <Switch>
       <Nav />
       <Route exact path='/' component={Home} />
-    </>
+    </Switch>
   );
 }
 
