464e7ec1cc05606b9c3335c232afe7aa84bb4dc8
verydecent <wnjhwng@gmail.com>
2020-06-09 14:11:26 -0700

    Remove nav component and add Routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../Home';
 import Nav from '../Nav';
+import Post from '../Post';
 
 const Routes = () => {
   return (
     <Switch>
-      <Nav />
       <Route exact path='/' component={Home} />
+      <Route path='/post' component={Post} /> 
     </Switch>
   );
 }
