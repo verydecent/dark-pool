6150ad80629669ede65da599876c5fed6b99c81d
verydecent <wnjhwng@gmail.com>
2020-06-10 15:38:30 -0700

    Import new components to Route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../Home';
 import Nav from '../Nav';
 import Post from '../Post';
+import Notes from '../Notes';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route path='/post' component={Post} /> 
+      <Route path='/post' component={Post} />
+      <Route path='/all' component={Notes} />
     </Switch>
   );
 }
