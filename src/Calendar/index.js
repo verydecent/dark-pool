5b8d7b13ff43114aae49261c7697fc4b191bda90
verydecent <wnjhwng@gmail.com>
2020-06-10 15:38:19 -0700

    Create boilerplates

---
+++ src/Calendar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Calendar extends React.Component {
+  
+
+  render() {
+    return (
+      <div>
+        <h2>Calendar</h2>
+      </div>
+    );
+  }
+}
