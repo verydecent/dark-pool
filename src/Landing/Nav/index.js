d138ca32c888a94aee6bfa970b1e1e378843cffc
Wonjae Hwang <wnjhwng@gmail.com>
2020-06-15 22:18:15 -0700

    remove files

--- src/Landing/Nav/index.js
+++ src/Landing/Nav/index.js
@@ -6,21 +6,16 @@ const Nav = () => {
   return (
     <div className='nav'>
       <div className='nav__global-logo'>
-        <Link to='/'>Logo Here</Link>
+        <Link to='/'>darkpoolnotes</Link>
       </div>
       <nav className='nav__menu'>
         <ul className='nav__menu__list'>
           <li><Link to='/'>Home</Link></li>
-          <li><Link to='/concept'>Concept</Link></li>
-          <li><Link to='/post'>Post</Link></li>
-          <li><Link to='/all'>See Notes</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/register'>Register</Link></li>
         </ul>
       </nav>
-      <nav className='nav__utility'>
-        <Link to='/log-in'>Log In</Link>
-        <Link to='/register'>Register</Link>
-      </nav>
-    </div>
+		</div>
   );
 }
 
