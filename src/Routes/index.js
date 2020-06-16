eec83f9f4b3252affccbbd7b2bccb35ef4ac4ae4
Wonjae Hwang <wnjhwng@gmail.com>
2020-06-15 22:06:34 -0700

    Name change

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from '../Front/Home';
+import Home from '../Landing/Home';
 
 const Routes = () => {
   return (
