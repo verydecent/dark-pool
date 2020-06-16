fed728ec97f07b4ea27a7d90a661f5ff87d9faa9
Wonjae Hwang <wnjhwng@gmail.com>
2020-06-16 00:58:07 -0700

    Update routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+// Landing
 import Home from '../Landing/Home';
 import Nav from '../Landing/Nav';
-
+// App
 import Dashboard from '../App/Dashboard';
+import SideNav from '../App/SideNav';
 
 const Routes = () => {
   return (
 		<>
 			<Nav />
 			<Switch>
+				// Landing
 				<Route exact path='/' component={Home} />
+				// App
 				<Route path='/app' component={Dashboard} />
+
 			</Switch>
 		</>
   );
