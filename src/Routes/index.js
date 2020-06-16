16ec9989e2391738d0cfed98e957761e039a622a
Wonjae Hwang <wnjhwng@gmail.com>
2020-06-15 22:01:49 -0700

    Update routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../Front/Home';
-import Post from '../App/Post';
-import Notes from '../App/Notes';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route path='/post' component={Post} />
-      <Route path='/all' component={Notes} />
-    </Switch>
+		</Switch>
   );
 }
 
