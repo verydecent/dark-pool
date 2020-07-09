467edf69bbb23e242ed134e7036f74daecd57ffb
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-08 17:14:06 -0700

    Update rough template

--- src/Components/Collection/index.js
+++ src/Components/Collection/index.js
@@ -13,7 +13,24 @@ class Collection extends React.Component {
   render() {
     return (
       <div className='collection'>
-        Collection Component Here
+        <div className='collection-container'>
+          <div className='collection-header-container'>
+            <h2 className='collection-title'>Collection</h2>
+            <button>Create Collection</button>
+          </div>
+         
+          <div className='collection-box-container'>
+
+            <div className='collection-box-container-header'>
+              <h2 className='date-header'>Wed July 8 2020</h2>
+              <div className='collection-carousel-buttons'>
+                <button>Left</button>
+                <button>right</button>
+              </div>
+            </div>
+            BOX BOX BOX BOX
+          </div>
+        </div>
       </div>
     );
   }
