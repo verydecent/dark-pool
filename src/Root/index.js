94d1ca6c915f262dd1f8ad3e306823b9b5ab1aa2
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-12 17:35:29 -0700

    Create global style component

--- src/Root/index.js
+++ src/Root/index.js
@@ -5,6 +5,25 @@ import './global.css';
 import './font.css';
 import './reset.css';
 import Routes from '../Routes';
+import RobotoRegular from '../../assets/fonts/Roboto-Regular.ttf';
+import RobotoMedium from '../../assets/fonts/Roboto-Medium.ttf';
+import RobotoBold from '../../assets/fonts/Roboto-Bold.ttf';
+
+const myGlobalCSS = `
+  @font-face {
+    font-family: 'Roboto',
+    font-style: normal,
+    font-weight: normal,
+    src:
+      url('${RobotoRegular}') format('ttf'),
+      url('${RobotoMedium}') format('ttf'),
+      url('${RobotoBold}') format('ttf'),
+  }
+
+  html, body {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
 
 const Root = () => {
   return (
