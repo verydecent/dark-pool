990061245d73db06a13d6988c09fd6b999bc0009
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-13 23:53:56 -0700

    add other fa icons

--- src/Components/FAIcons/index.js
+++ src/Components/FAIcons/index.js
@@ -5,8 +5,23 @@ import './styles.css';
 export const Times = () => {
   return (
     <div className='button-icon'>
-      <FontAwesomeIcon icon='times-circle' color='rgba(255, 255, 255, 0.6)'/>
+      <FontAwesomeIcon icon='times' color='rgba(255, 255, 255, 0.6)'/>
     </div>
   );
 }
 
+export const TimesCircle = () => {
+return (
+  <div className='button-icon'>
+    <FontAwesomeIcon icon='times-circle' color='rgba(255, 255, 255, 0.6)'/>
+  </div>
+  );
+}
+
+export const WindowClose = () => {
+  return (
+    <div className='button-icon'>
+      <FontAwesomeIcon icon='window-close' color='rgba(255, 255, 255, 0.6)'/>
+    </div>
+  );
+}
