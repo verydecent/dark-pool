54e7a41053c08128f9b021cd8a64dece0e2ba35e
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-13 19:04:28 -0700

    delete spaces

--- src/Components/CollectionBox/index.js
+++ src/Components/CollectionBox/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './styles.css';
 
 const CollectionBox = ({
-
   toggleModal
-
 }) => {
   return (
     <div className='collection-box' onClick={() => toggleModal()}>
