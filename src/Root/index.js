2eb93769f4abfa880179948a374e09d4719681dd
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-13 18:33:03 -0700

    Remove font file no longer existant

--- src/Root/index.js
+++ src/Root/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './global.css';
-import './font.css';
 import './reset.css';
 import Routes from '../Routes';
 
