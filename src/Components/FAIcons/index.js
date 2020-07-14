2eaf19b77f479c98be6e6b75e233476972859624
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-13 23:50:57 -0700

    add hovering effect for clickable icons, className === 'button-icon'

--- src/Components/FAIcons/index.js
+++ src/Components/FAIcons/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './styles.css';
 
-export const Times = () => <FontAwesomeIcon icon='times-circle' color='rgba(255, 255, 255, 0.6)'/>;
+export const Times = () => {
+  return (
+    <div className='button-icon'>
+      <FontAwesomeIcon icon='times-circle' color='rgba(255, 255, 255, 0.6)'/>
+    </div>
+  );
+}
 
