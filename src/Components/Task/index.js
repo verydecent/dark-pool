97a791e6adfa86fb57d1e021460776d014520001
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-18 00:29:06 -0700

    Add taskTitle to Task viewing component

--- src/Components/Task/index.js
+++ src/Components/Task/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles.css';
 
 const Task = ({
+  taskTitle,
+  taskDescription,
   toggleModal
 }) => {
   return (
     <div className='collection-box' onClick={() => toggleModal()}>
       <div className='collection-box-container'>
-        Task Component!
+        {taskTitle}
       </div>
     </div>
   );
