0653ba59bff6131194409a76cff3e693b6a76658
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-21 23:30:48 -0700

    Add comments to seperate props as values and methods, a thing I've been doing lately and it makes the code more readable and easier to consume

--- src/Components/Task/index.js
+++ src/Components/Task/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './styles.css';
 
 const Task = ({
+  // Values
   id,
   title,
   description,
   subtasks,
+
+  // Methods
   toggleModal,
   selectTask
 }) => {
