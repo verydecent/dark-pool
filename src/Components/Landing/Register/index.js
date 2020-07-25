70b60f73e7f90a9b015642b9275a44aa8b0a21d9
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-25 02:32:03 -0700

    set up boiler plate for registration component

--- src/Components/Landing/Register/index.js
+++ src/Components/Landing/Register/index.js
@@ -2,13 +2,63 @@ import React from 'react';
 import withNav from '../Hoc/withNav';
 
 class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value }, console.log(this.state));
+  }
+
   render() {
     return (
       <>
         Register Component
-      </>
+        <form className=''>
+          <input
+           name='firstName'
+           placeholder='First Name'
+           type='text'
+           onChange={(e) => this.handleChange(e)}
+          />
+          <input
+           name='lastName'
+           placeholder='Last Name'
+           type='text'
+           onChange={(e) => this.handleChange(e)}
+          />
+         <input
+           name='email'
+           placeholder='Email'
+           type='text'
+           onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            name='password'
+            placeholder='Password'
+            type='text'
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+           name='confirmPassword'
+           placeholder='Confirm Password'
+           type='text'
+           onChange={(e) => this.handleChange(e)}
+          />
+          <button>Log in</button>
+        </form>
+     </>
     );
   }
 }
+
 
 export default withNav(Register);
