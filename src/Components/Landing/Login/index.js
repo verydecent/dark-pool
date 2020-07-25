435ec8a37c7aec91937b3027aaa8eb6344f4e506
Wonjae Hwang <wnjhwng@gmail.com>
2020-07-25 02:29:11 -0700

    create form

--- src/Components/Landing/Login/index.js
+++ src/Components/Landing/Login/index.js
@@ -2,14 +2,41 @@ import React from 'react';
 import withNav from '../Hoc/withNav';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value }, console.log(this.state));
+  }
+
   render() {
     return (
       <>
         Login Component
+        <form className=''>
+         <input
+           name='username'
+           placeholder='username'
+           type='text'
+           onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            name='password'
+            placeholder='password'
+            type='password'
+            onChange={(e) => this.handleChange(e)}
+          />
 
-        <input placeholder='username' type='text' />
-        <input placeholder='password' type='text' />
-      </>
+          <button>Log in</button>
+        </form>
+     </>
     );
   }
 }
