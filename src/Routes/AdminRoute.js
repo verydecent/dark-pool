0e82126d7a670de2bd7bfd0195268b37836a2bb5
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-12 14:53:38 -0700

    Create Admin Protected Route

---
+++ src/Routes/AdminRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from '../Utilities/helpers';
+
+const AdminRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render ={
+    props => isAuthenticated() && isAuthenticated().role === 'admin'
+      ? <Component {...props} />
+      : <Redirect to={{
+          pathName: '/login',
+          state: {
+            from: props.location
+          }
+        }} />
+  } />
+);
+
+export default AdminRoute;
