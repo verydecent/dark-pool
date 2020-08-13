a82ec76ceb4ffbb43056ec2a5bc32e0cf4d50688
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-13 13:40:46 -0700

    Create Forgot password option view

---
+++ src/Components/Landing/ForgotPassword/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+
+class ForgotPassword extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email } = this.state;
+    
+    axios.post(`${process.env.API_URL}/auth/forgot-password`, { email })
+      .then(response => {
+        console.log('Forgot password Request Success', response);
+      })
+      .catch(error => {
+        console.log('Forgot password Request Error', error);
+      });
+  }
+
+  render() {
+    const { email } = this.state;
+    return (
+      <>
+        <h1>Forgot password</h1>
+
+        <form className='' onClick={(e) => this.handleSubmit(e)}>
+          <input 
+            name='email'
+            value={email}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <button>Submit</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default ForgotPassword;
