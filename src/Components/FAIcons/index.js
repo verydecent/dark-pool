e372b356ea80aaf94306c4cfabcc0ef8bdf8ac4b
Wonjae Hwang <wnjhwng@gmail.com>
2020-08-18 23:06:50 -0700

    Add to FAIcons file

--- src/Components/FAIcons/index.js
+++ src/Components/FAIcons/index.js
@@ -41,3 +41,35 @@ export const CalendarAlt = () => {
     </div>
   );
 }
+
+export const Clipboard = () => {
+  return (
+    <div className='button-icon'>
+      <FontAwesomeIcon icon='clipboard' />
+    </div>
+  );
+}
+
+export const User = () => {
+  return (
+    <div className='button-icon'>
+      <FontAwesomeIcon icon='user' />
+    </div>
+  );
+}
+
+export const ChartLine = () => {
+  return (
+    <div className='button-icon'>
+      <FontAwesomeIcon icon='chart-line' />
+    </div>
+  );
+}
+
+export const SignOut = () => {
+  return (
+    <div className='button-icon'>
+      <FontAwesomeIcon icon='sign-out-alt' />
+    </div>
+  );
+}
