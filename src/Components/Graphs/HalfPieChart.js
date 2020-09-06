1ee98d08444a4b338b4ec8af15473cb5e41fae5e
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-06 07:48:49 -0700

    Put PieChart into its own file

---
+++ src/Components/Graphs/HalfPieChart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Label,
+  Text
+} from 'recharts';
+
+const data = [{ name: 'Group A', value: .5 }, { name: 'Group B', value: .5 }];
+const COLORS = ['#0088FE', 'transparent'];
+
+const HalfPieChart = () => {
+  return (
+    <PieChart width={800} height={400}>
+      <Pie
+        data={data}
+        cx={200}
+        cy={200}
+        startAngle={180}
+        endAngle={0}
+        innerRadius={60}
+        outerRadius={80}
+        // fill="#8884d8"
+        paddingAngle={0}
+      >
+        {
+          data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+        }
+        <Label value="Center" offset={0} position="center" />
+
+      </Pie>
+    </PieChart >
+  );
+}
+
+export default HalfPieChart;
