da6ea6cdd6708c37f3458a262e871373dba0edaa
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-06 07:19:41 -0700

    Readjust placement and styling of app-view

--- src/Components/Hoc/withNav.js
+++ src/Components/Hoc/withNav.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SideNav from '../SideNav';
+import HeaderNav from '../HeaderNav';
 import './styles.css';
 
 const withNav = Component => (props) => {
 	return (
 		<div className='app-view'>
-			<SideNav {...props} />
+			<HeaderNav />
 			<div className='main-view'>
+				<SideNav {...props} />
 				<Component />
 			</div>
 		</div>
