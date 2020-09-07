0cac7380a96424653c296d7a80834197224e92c2
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-07 08:20:08 -0700

    Remove label css

--- src/Components/TaskModal/Details.js
+++ src/Components/TaskModal/Details.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
+// Need to build helper file for getting
+
 const Details = ({
 
 }) => {
   return (
     <div className='task-modal-details'>
-      Details
+      <label>Details</label>
+      {/* Details */}
+      <ol>
+        <li>
+          <div className='task-modal-details-group'>
+            <span className='task-modal-details-group-field'>Subtasks Remaining:</span>
+            <span className='task-modal-details-group-data'>25</span>
+          </div>
+        </li>
+        <li>
+          <div className='task-modal-details-group'>
+            <span className='task-modal-details-group-field'>Subtasks Complete:</span>
+            <span className='task-modal-details-group-data'>75</span>
+          </div>
+        </li>
+        <li>
+          <div className='task-modal-details-group'>
+            <span className='task-modal-details-group-field'>Subtasks Total:</span>
+            <span className='task-modal-details-group-data'>100</span>
+          </div>
+        </li>
+      </ol>
+      {/* Graph */}
+      <div className='task-modal-details-graph'>
+
+      </div>
     </div>
   );
 }
