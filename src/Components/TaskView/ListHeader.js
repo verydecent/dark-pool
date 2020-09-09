3a03da1b98b67335d4fa30706ea0216c03401a29
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-09 05:43:16 -0700

    Componentize ListHeader section

---
+++ src/Components/TaskView/ListHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ListHeader = () => {
+  return (
+    <div className='task-view-field-header'>
+      <div className='task-view-field'>Task Title</div>
+      <div className='task-view-field'>Subtask Total</div>
+      <div className='task-view-field'>Remaining Subtasks</div>
+      <div className='task-view-field'>Percent Complete</div>
+    </div>
+  );
+}
+
+export default ListHeader;
