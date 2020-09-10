c8cec3e70e6417713ad6f3b1db615844fccb9696
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-10 00:14:41 -0700

    Change class name

--- src/Components/DashboardView/index.js
+++ src/Components/DashboardView/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import SideNav from '../SideNav';
 import withNav from '../Hoc/withNav';
 import './styles.css';
+import { AreaChart, Area, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
-class Dashboard extends React.Component {
+// Data
+
+const data = [
+  { month: 'Jan', completed: 1 },
+  { month: 'Feb', completed: .9 },
+  { month: 'Mar', completed: .80 },
+  { month: 'Apr', completed: .20 },
+  { month: 'May', completed: .70 },
+  { month: 'Jun', completed: .60 },
+  { month: 'Jul', completed: .90 },
+  { month: 'Aug', completed: .80 },
+  { month: 'Sep', completed: .40 },
+  { month: 'Oct', completed: .12 },
+  { month: 'Nov', completed: .45 },
+  { month: 'Dec', completed: .100 },
+];
+
+// Change Y Axis into percent
+const toPercent = (decimal, fixed = 0) => {
+  return `${(decimal * 100).toFixed(fixed)}%`;
+};
+
+class DashboardView extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -17,48 +39,25 @@ class Dashboard extends React.Component {
 
 
 
-        Todo list
-        <br />
-        <br />
-        Frontend:
-        <br />
-        Error handling requests and login/registration form
-        <br />
-        Password confirmation for account and registration....
-        <br />
-
-        Come back for detail padding and font sizing and UI colors later, first get everything into place
-        <br />
-
-        In order from easiest to most complex and time consuming
-        Account Settings
-        Header Bar
-        Side Nav
-        Landing
-        Dashboard
-        Calendar
-
-        <br />
-        Possible ideas:
-        <br />
-        Add a pie graph on the top right side of Tasks to show how many tasks are finished
-        <br />
-
-
-        <br />
-
-
-        Backend:
-        <br />
-        Validating required info such as user_id
-        Adding error response
-
-
-
-
+        {/* Graph */}
+        <div style={{
+          border: '1px solid green'
+        }}>
+          <AreaChart
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            width={500}
+            height={500}
+            data={data}
+          >
+            <XAxis dataKey="month" />
+            <YAxis tickFormatter={toPercent} />
+            <Tooltip />
+            <Area type='monotone' dataKey='completed' stroke='#000' fill='#E44B6F' />
+          </AreaChart>
+        </div>
       </div>
     );
   }
 }
 
-export default withNav(Dashboard);
+export default withNav(DashboardView);
