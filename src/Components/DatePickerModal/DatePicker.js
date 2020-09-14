dbf52269de24f56fa0233d4ad9aef16ebd9be0d8
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-14 05:17:25 -0700

    Set up boilerplate for DatePicker Component

---
+++ src/Components/DatePickerModal/DatePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DatePicker = ({
+
+}) => {
+  return (
+    <div className='date-picker'>
+      <div className='date-picker-container'>
+        <input type='date' />
+      </div>
+    </div>
+  );
+}
+
+export default DatePicker;
