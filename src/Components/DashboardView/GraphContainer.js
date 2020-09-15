74307a9f14d6618f0af190e353546f29fd9c02c7
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-15 11:50:36 -0700

    Add props

--- src/Components/DashboardView/GraphContainer.js
+++ src/Components/DashboardView/GraphContainer.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Day from '../Graphs/Day';
 
 const GraphContainer = ({
-  graphTimeFrame
+  // Values
+  tasks,
+  graphTimeFrame,
+  // Methods
 }) => {
   return (
     <div className='dashboard-view-graph-container'>
-      {graphTimeFrame === 'day' && <Day />}
+      {graphTimeFrame === 'day' && <Day tasks={tasks} />}
       {graphTimeFrame === 'isoWeek' && <Day />}
       {graphTimeFrame === 'month' && <Day />}
       {graphTimeFrame === 'year' && <Day />}
