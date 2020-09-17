381899f1ec2281a51cfb71b87488797d2001a6db
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-17 05:40:15 -0700

    Comment out

--- src/Components/Graphs/Month/index.js
+++ src/Components/Graphs/Month/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { barChartDayData, lineChartDayData, areaChartDayData } from './helpers';
-import { LineGraph, BarGraph, AreaGraph } from '../Day/graphs';
+
 
 const MonthContainer = ({
   tasks,
   graphType
 }) => {
-  const lineData = lineChartDayData(tasks);
-  const barData = barChartDayData(tasks);
-  const areaData = areaChartDayData(tasks);
 
-  const LineChartConditional = graphType === 'line' ? <LineGraph data={lineData} /> : null;
-  const BarChartConditional = graphType === 'bar' ? <BarGraph data={barData} /> : null;
-  const AreaChartConditional = graphType === 'area' ? <AreaGraph data={areaData} /> : null;
+  // const LineChartConditional = graphType === 'line' ? <LineGraph data={lineData} /> : null;
+  // const BarChartConditional = graphType === 'bar' ? <BarGraph data={barData} /> : null;
+  // const AreaChartConditional = graphType === 'area' ? <AreaGraph data={areaData} /> : null;
 
   return (
     <div className=''>
-      {LineChartConditional}
+      {/* {LineChartConditional}
       {BarChartConditional}
-      {AreaChartConditional}
+      {AreaChartConditional} */}
     </div>
   )
 }
