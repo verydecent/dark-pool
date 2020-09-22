3cf725865915583734651068801cf2cc4da78691
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-22 12:01:45 -0700

    redux reducer boilerplate

---
+++ src/Redux/reducer.js
@@ -0,0 +1,11 @@
+function reducer(state, action) {
+  switch (action.type) {
+    case 'TOGGLE_ACCOUNT_MODAL': {
+      // Do something with action
+    }
+    case 'TOGGLER': {
+      // Do something with action
+    }
+    default: return state
+  }
+}
