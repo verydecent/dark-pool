39a7bc9da68c63baa8a5b5a907ee007cb048043b
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-24 13:58:44 -0700

    Set up boilerplate for account modal toggler

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,11 +1,18 @@
+import { TOGGLE_ACCOUNT_MODAL } from './constants';
+
+const toggleAccountModal = state => {
+  return {
+    isAccountModalOpen: !state.isAccountModalOpen
+  }
+}
+
 function reducer(state, action) {
   switch (action.type) {
-    case 'TOGGLE_ACCOUNT_MODAL': {
-      // Do something with action
-    }
-    case 'TOGGLER': {
-      // Do something with action
+    case TOGGLE_ACCOUNT_MODAL: {
+      return toggleAccountModal(state);
     }
     default: return state
   }
 }
+
+export default reducer;
