1fd30da7f117ca862103d4abd12aaeb3ff7382be
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-24 14:32:42 -0700

    Remove css and jsx

--- src/Components/HeaderNav/index.js
+++ src/Components/HeaderNav/index.js
@@ -4,21 +4,26 @@ import './styles.css';
 import { connect } from 'react-redux';
 import { toggleAccountModal } from '../../Redux/Actions';
 
-const mapDispatchToState = dispatch => dispatch(toggleAccountModal())
+const mapStateToProps = {};
 
-const HeaderNav = () => {
+const mapDispatchToProps = dispatch => {
+  console.log('dispatch', dispatch(toggleAccountModal()));
+  return {
+    toggleAccountModal: () => dispatch(toggleAccountModal())
+  }
+};
+
+const HeaderNav = (props) => {
+  console.log(props.toggleAccountModal())
   return (
     <div className='header-nav'>
       <div className='header-nav-top'>
-        <div className='header-nav-user-icon'>
+        <div className='header-nav-user-icon' onClick={() => props.toggleAccountModal()}>
           <User />
         </div>
       </div>
-      {/* <div className='header-nav-bottom'>
-
-      </div> */}
     </div>
   );
 }
 
-export default connect(null, mapDispatchToState)(HeaderNav);
+export default connect(null, mapDispatchToProps)(HeaderNav);
