64bfc2aeec96199721b486592c803ac5c48e15cc
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-25 18:25:52 -0700

    boiler plate for calendarModal

---
+++ src/Components/CalendarModal/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './styles.css';
+
+class CalendarModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className=''>
+
+      </div>
+    );
+  }
+}
+
+export default CalendarModal;
