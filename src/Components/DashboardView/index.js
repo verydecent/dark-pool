77bbca00315155b5123329263688c3de23172ea0
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-30 12:51:17 -0700

    Create Remove all bindings and methods that have to do with DatePickerModal and remove component

--- src/Components/DashboardView/index.js
+++ src/Components/DashboardView/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import withNav from '../Hoc/withNav';
 import './styles.css';
 import { isAuthenticated } from '../../Utilities/helpers';
-import Day from '../Graphs/Day';
-import DatePickerModal from '../DatePickerModal';
 import moment from 'moment';
-import shortid from 'shortid';
 import axios from '../../Utilities/axiosConfig';
 import GraphContainer from './GraphContainer';
 import { TaskGauge, SubtaskGauge } from '../Graphs/dashboardGraphs';
@@ -92,12 +89,6 @@ class DashboardView extends React.Component {
     super();
     this.state = {
       userId: isAuthenticated()._id,
-
-      // View
-      isModalVisible: false,
-      isDateTableVisible: true,
-      isYearTableVisible: false,
-      isMonthTableVisible: false,
       // Graph
       timeFrame: 'day',
       graphType: 'line',
@@ -106,7 +97,6 @@ class DashboardView extends React.Component {
 
       // Date Picker
       dateObject: moment(),
-      selectedDay: '',
       beginning: '',
       end: ''
     };
@@ -117,28 +107,6 @@ class DashboardView extends React.Component {
     // Graph methods
     this.selectTimeFrame = this.selectTimeFrame.bind(this);
     this.selectGraphType = this.selectGraphType.bind(this);
-
-    // View methods
-    this.toggleModal = this.toggleModal.bind(this);
-    this.toggleMonthTable = this.toggleMonthTable.bind(this);
-    this.toggleYearTable = this.toggleYearTable.bind(this);
-
-
-    // Date Picker methods
-    this.getFirstDayOfMonth = this.getFirstDayOfMonth.bind(this);
-    this.getWeekdays = this.getWeekdays.bind(this);
-    this.getAllMonths = this.getAllMonths.bind(this);
-    this.getDaysInMonth = this.getDaysInMonth.bind(this);
-    this.getToday = this.getToday.bind(this);
-    this.getMonth = this.getMonth.bind(this);
-    this.setMonth = this.setMonth.bind(this);
-    this.getYear = this.getYear.bind(this);
-    this.getDates = this.getDates.bind(this);
-    this.yearTable = this.yearTable.bind(this);
-    this.setYear = this.setYear.bind(this);
-    this.onPrev = this.onPrev.bind(this);
-    this.onNext = this.onNext.bind(this);
-    this.onDayClick = this.onDayClick.bind(this);
   }
 
   componentDidMount() {
@@ -203,193 +171,6 @@ class DashboardView extends React.Component {
         end: end,
       }
     }, () => this.getTasks());
-  }
-
-  toggleModal() {
-    this.setState({
-      ...this.state,
-      isModalVisible: !this.state.isModalVisible
-    });
-  }
-
-  toggleMonthTable() {
-    this.setState({
-      // ...this.state,
-      isMonthTableVisible: !this.state.isMonthTableVisible,
-      isDateTableVisible: !this.state.isDateTableVisible
-    });
-  }
-
-  toggleYearTable() {
-    this.setState({
-      ...this.state,
-      isYearTableVisible: !this.state.isYearTableVisible,
-      isDateTableVisible: !this.state.isDateTableVisible
-    });
-  }
-
-  // Date Picker methods
-  getFirstDayOfMonth() {
-    const { dateObject } = this.state;
-    const firstDayOfMonth = moment(dateObject).startOf('month').format('d');
-
-    return firstDayOfMonth;
-  }
-
-  getWeekdays() {
-    const weekdays = moment.weekdaysShort();
-
-    return weekdays;
-  }
-
-  getAllMonths() {
-    const months = moment.months();
-
-    return months;
-  }
-
-  getDaysInMonth() {
-    const { dateObject } = this.state;
-    const daysInMonth = dateObject.daysInMonth();
-
-    return daysInMonth;
-  }
-
-  getToday() {
-    const { dateObject } = this.state;
-    const today = dateObject.format('D');
-
-    return today;
-  }
-
-  getMonth() {
-    return this.state.dateObject.format('MMMM');
-  }
-
-  getYear() {
-    return this.state.dateObject.format('Y');
-  }
-
-  getNextTenYears() {
-    return moment().set('year',)
-  }
-
-  setMonth(month) {
-    const monthNumber = this.getAllMonths().indexOf(month);
-    let dateObject = Object.assign({}, this.state.dateObject);
-    dateObject = moment(dateObject).set('month', monthNumber);
-
-    this.setState({
-      ...this.state,
-      dateObject: dateObject,
-      isMonthTableVisible: !this.state.isMonthTableVisible,
-      isDateTableVisible: !this.state.isDateTableVisible
-    });
-  }
-
-  getDates(startDate, endDate) {
-    const dateArray = [];
-    let currentDate = moment(startDate);
-    const stopDate = moment(endDate);
-    while (currentDate <= stopDate) {
-      dateArray.push(moment(currentDate).format('YYYY'));
-      currentDate = moment(currentDate).add(1, 'year');
-    }
-
-    return dateArray;
-  }
-
-  // Produces JSX for year table
-
-  yearTable(year) {
-    const nextTen = moment().set('year', year).add(12, 'year').format('Y');
-
-    const twelveYears = this.getDates(year, nextTen);
-
-    const months = twelveYears.map(year => {
-      return (
-        <td
-          key={shortid.generate()}
-          className=''
-          onClick={e => {
-            this.setYear(year)
-          }}
-        >
-          {year}
-        </td>
-      );
-    });
-
-    let rows = [];
-    let cells = [];
-
-    months.forEach((row, i) => {
-      if (i % 3 !== 0 || i == 0) {
-        cells.push(row);
-      }
-      else {
-        rows.push(cells);
-        cells = [];
-        cells.push(row);
-      }
-    });
-    rows.push(cells);
-
-    const YearList = rows.map((d, i) => <tr key={shortid.generate()}>{d}</tr>);
-
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th colSpan='4'>Select a Year</th>
-          </tr>
-        </thead>
-        <tbody>{YearList}</tbody>
-      </table>
-    );
-  }
-
-  setYear(year) {
-    let dateObject = Object.assign({}, this.state.dateObject);
-    dateObject = moment(dateObject).set('year', year);
-    this.setState({
-      isYearTableVisible: !this.state.isYearTableVisible,
-      isMonthTableVisible: !this.state.isMonthTableVisible,
-      dateObject: dateObject
-    });
-  }
-
-  onPrev() {
-    let curr = '';
-    if (this.state.isMonthTableVisible) {
-      curr = 'month';
-    }
-    else {
-      curr = 'year';
-    }
-    this.setState({
-      dateObject: this.state.dateObject.subtract(1, curr)
-    });
-  }
-
-  onNext() {
-    let curr = '';
-    if (this.state.isMonthTableVisible) {
-      curr = 'month';
-    }
-    else {
-      curr = 'year';
-    }
-    this.setState({
-      dateObject: this.state.dateObject.add(1, curr)
-    });
-  }
-
-  onDayClick(e, d) {
-    this.setState({
-      ...this.state,
-      selectedDay: d
-    }, () => console.log('Selected day', d));
   }
 
   render() {
@@ -517,28 +298,6 @@ class DashboardView extends React.Component {
           </div>
         </div>
         <div className='dashboard-view'>
-          <DatePickerModal
-            isModalVisible={this.state.isModalVisible}
-            isDateTableVisible={this.state.isDateTableVisible}
-            isMonthTableVisible={this.state.isMonthTableVisible}
-            isYearTableVisible={this.state.isYearTableVisible}
-            toggleModal={this.toggleModal}
-            toggleMonthTable={this.toggleMonthTable}
-            toggleYearTable={this.toggleYearTable}
-            getFirstDayOfMonth={this.getFirstDayOfMonth}
-            getWeekdays={this.getWeekdays}
-            getAllMonths={this.getAllMonths}
-            getDaysInMonth={this.getDaysInMonth}
-            getToday={this.getToday}
-            getMonth={this.getMonth}
-            getYear={this.getYear}
-            setMonth={this.setMonth}
-            onPrev={this.onPrev}
-            onNext={this.onNext}
-            onDayClick={this.onDayClick}
-            // Prouces JSX
-            yearTable={this.yearTable}
-          />
           <div className='dashboard-view-container'>
             <div className='dashboard-view-header'>
               <h1 className='header-1'>
