079f2a4a3d4ed7b429b95e77fcceda0cff34a1d6
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-30 13:28:53 -0700

    Create action for setToCurrentdate

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
+import moment from 'moment';
 
 const initialState = {
   isAccountModalOpen: false,
   isCalendarModalOpen: false,
+  dateContext: moment()
 }
 
 const store = createStore(
