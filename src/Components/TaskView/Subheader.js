6f922c79f57d7b7e37b1d9bc7ef4db3b5337746a
Wonjae Hwang <wnjhwng@gmail.com>
2020-09-29 19:58:58 -0700

    Set up button for toggling calendar modal

--- src/Components/TaskView/Subheader.js
+++ src/Components/TaskView/Subheader.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import Button from '../Button';
+import { Calendar } from '../FAIcons';
 
 const Subheader = ({
   date,
-  createTask,
   parseNextDate,
-  parsePrevDate
+  parsePrevDate,
+  toggleCalendarModal
 }) => {
+
+  console.log(toggleCalendarModal)
   const formatDate = date.format('dddd LL');
   return (
     <div className='task-view-subheader'>
-      <span className='task-view-date-title'>{formatDate}</span>
+      <span className='task-view-date-title'>
+        <Button onClick={toggleCalendarModal}>
+          <Calendar />
+        </Button>
+        {formatDate}
+      </span>
       <div>
         <Button onClick={parsePrevDate}>
           Yesterday
-      </Button>
+        </Button>
         <Button onClick={parseNextDate}>
           Tomorrow
-      </Button>
+        </Button>
       </div>
     </div>
   );
