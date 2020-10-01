96ac3e351196bcc9274ad388ad45519c22f13361
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-01 16:04:06 -0700

    Own styling for task buttons:

--- src/Components/TaskView/Subheader.js
+++ src/Components/TaskView/Subheader.js
@@ -10,16 +10,16 @@ const Subheader = ({
   return (
     <div className='task-view-subheader'>
       <div className='task-view-button-container'>
-        <button className='grey-button' onClick={parsePrevDate}>
+        <button className='task-view-button' onClick={parsePrevDate}>
           <AngleLeft />
         </button>
-        <button className='grey-button' onClick={parseNextDate}>
+        <button className='task-view-button' onClick={parseNextDate}>
           <AngleRight />
         </button>
-        <button className='grey-button' onClick={toggleCalendarModal}>
+        <button className='task-view-button' onClick={toggleCalendarModal}>
           <Calendar />
         </button>
-        <button className='grey-button plus' onClick={createTask}>
+        <button className='task-view-button plus' onClick={createTask}>
           <Plus />
         </button>
       </div>
