e2b4a69286347834925092a166f13b914acc82c3
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-01 17:03:39 -0700

    import isAuth helper to intialize user id

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,13 +2,16 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
 import moment from 'moment';
 import logger from 'redux-logger';
+import { isAuthenticated } from '../Utilities/helpers';
 
 const initialState = {
+  // Global
+  userId: isAuthenticated()._id,
+  dateContext: moment(),
   // Account Modal
   isAccountModalOpen: false,
   // Calendar Modal
   isCalendarModalOpen: false,
-  dateContext: moment(),
   isMonthListOpen: false,
   isYearListOpen: false,
 }
