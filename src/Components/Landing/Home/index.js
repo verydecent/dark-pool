a4e48f0fe18fe8e75cdf58f12b63361c2f02d081
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-04 17:07:52 -0700

    Implement react wavifiy

--- src/Components/Landing/Home/index.js
+++ src/Components/Landing/Home/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import withNav from '../Hoc/withNav';
+import Register from '../Register';
+import Wave from 'react-wavify';
+import './styles.css';
 
 const Home = () => {
   return (
-    <div className='home'>
-			Home Page Component Here
+    <div className='home-wrapper'>
+      <div className='home-hero-section'>
+        <h1 className='home-title'>
+          DarkPool
+        </h1>
+        <p className='home-description'>
+          Brainstorm Organize Reflect
+        </p>
+      </div>
+      {/* Waves */}
+      <div className='wave-section'>
+        <Wave fill='#418BCA'
+          paused={false}
+          options={{
+            amplitude: 25,
+            speed: .5,
+            points: 3
+          }}
+        />
+      </div>
     </div>
   );
 }
