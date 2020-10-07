e6b19c7a2b6846a7d2c5a37aee5755a7a82ff631
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-07 15:31:36 -0700

    Error handler

--- create-env.js
+++ create-env.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
-fs.writeFileSync('./.env', `API_URL=${process.env.API_URL}\nNODE_ENV=production`)
-console.log('process.env', process.env.API_URL)
-console.log(fs);
+fs.writeFileSync('./.env', `API_URL=${process.env.API_URL}\nNODE_ENV=production`, function (err) {
+  console.log('===> writeFileSync Err');
+  if (err) throw err;
+});
