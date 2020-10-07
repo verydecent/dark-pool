178f0c57bf7b4354a7d6ce6966925d3fd53826fe
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-07 15:36:47 -0700

    dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require("webpack");
 const Dotenv = require('dotenv-webpack');
 
+
 module.exports = {
   mode: "production",
   entry: "./src/Root/index.js",
@@ -33,16 +34,17 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      PRODUCTION: JSON.stringify(true),
-      VERSION: JSON.stringify('5fa3b9'),
-      BROWSER_SUPPORTS_HTML5: true,
-      TWO: '1+1',
-      'typeof window': JSON.stringify('object'),
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    })
+    new Dotenv()
+    // new webpack.DefinePlugin({
+    //   PRODUCTION: JSON.stringify(true),
+    //   VERSION: JSON.stringify('5fa3b9'),
+    //   BROWSER_SUPPORTS_HTML5: true,
+    //   TWO: '1+1',
+    //   'typeof window': JSON.stringify('object'),
+    //   'process.env': {
+    //     NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    //   }
+    // })
   ],
   devServer: {
     contentBase: "./build",
