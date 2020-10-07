57850e89552fcc2de16f2aba4c606fe5c19ce344
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-06 21:26:34 -0700

    remove period

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new Dotenv({
-      path: path.resolve(__dirname + './.env')
+      path: path.resolve(__dirname + '/.env')
     })
   ],
   devServer: {
