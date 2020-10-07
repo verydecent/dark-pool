8d46dd67b5bff9272396c5f5625616c913a4dc1d
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-07 15:33:39 -0700

    update

--- create-env.js
+++ create-env.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-fs.writeFileSync('./.env', `API_URL=${process.env.API_URL}\nNODE_ENV=production`, function (err) {
+fs.writeFileSync('./.env', `API_URL=${process.env.API_URL}`, function (err) {
   console.log('===> writeFileSync Err');
   if (err) throw err;
 });
