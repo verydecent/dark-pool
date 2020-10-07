0bcff75df7dcef00cb97e121aac05bbfb4b5152d
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-06 17:14:44 -0700

    Remove scripts and add query

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
+        use: ["babel-loader", "eslint-loader"],
+        query: require('./.babelrc')
       },
       {
         test: /\.css$/i,
