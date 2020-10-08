8f67f37baba676daf8e438b003177866d197add7
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-08 14:31:04 -0700

    Create boilerplate for authmessage

---
+++ src/Components/AuthModal/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const AuthModal = ({
+  message
+}) => {
+  return (
+    <div className=''>
+      {message}
+    </div>
+  );
+}
+
+export default AuthModal;
