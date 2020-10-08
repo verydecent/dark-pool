7f0d20eee772a37e064d7119b267f79a59bb6350
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-08 14:34:03 -0700

    Copy styles

--- src/Components/AuthModal/index.js
+++ src/Components/AuthModal/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './styles.css';
 
 const AuthModal = ({
   message
 }) => {
   return (
-    <div className=''>
-      {message}
+    <div className='auth-modal'>
+      <div className='auth-modal-overlay' onClick={() => { }}></div>
+      <div className='auth-modal-view'>
+
+        {message}
+      </div>
     </div>
   );
 }
+
+// const mapStateToProps = state => 
 
 export default AuthModal;
