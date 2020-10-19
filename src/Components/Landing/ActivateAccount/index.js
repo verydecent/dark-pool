ed5c9b94d25789b7873019a8dbb7ab3a3ada9177
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-19 09:58:16 -0700

    Remove jwt

--- src/Components/Landing/ActivateAccount/index.js
+++ src/Components/Landing/ActivateAccount/index.js
@@ -1,51 +1,57 @@
-import React from 'react';
-import axios from 'axios';
-import withNav from '../Hoc/withNav';
-import jwt from 'jsonwebtoken';
+import React from "react";
+import axios from "axios";
+import withNav from "../Hoc/withNav";
+import jwt from "jsonwebtoken";
 
 class ActivateAccount extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      token: '',
+      username: "",
+      token: "",
       visible: true,
-      buttonText: 'Activate Account'
-    }
+      buttonText: "Activate Account",
+    };
   }
 
   componentDidMount() {
     const token = this.props.match.params.token;
     const { username } = jwt.decode(token);
-    console.log(token);
     if (token) this.setState({ username, token });
   }
 
   handleSubmit(e) {
     const { token } = this.state;
 
-    this.setState({ buttonText: 'Activating...' });
+    this.setState({ buttonText: "Activating..." });
 
-    axios.post(`${process.env.API_URL}/auth/account-activation`, {
-      token
-    })
-    .then(response => {
-      console.log('Account Activation Success', response);
-      this.setState({ visible: false, buttonText: 'Account Activated' });
-    })
-    .catch(error => {
-      console.log('Account Activation Error', error);
-    });
+    axios
+      .post(`${process.env.API_URL}/auth/account-activation`, {
+        token,
+      })
+      .then((response) => {
+        console.log("Account Activation Success", response);
+        this.setState({ visible: false, buttonText: "Account Activated" });
+      })
+      .catch((error) => {
+        console.log("Account Activation Error", error);
+      });
   }
 
   render() {
     const { username, visible, buttonText } = this.state;
 
-    const title = visible ? <h1>Hey, {username}, Are you ready to activate your account?</h1> : <h1>Your account was successfully activated! You can now login!</h1>;
+    const title = visible ? (
+      <h1>Hey, {username}, Are you ready to activate your account?</h1>
+    ) : (
+      <h1>Your account was successfully activated! You can now login!</h1>
+    );
     return (
       <>
         {title}
-        <button disabled={!visible} onClick={(e) => this.handleSubmit(e)}>{buttonText}</button>
+        <button disabled={!visible} onClick={(e) => this.handleSubmit(e)}>
+          {buttonText}
+        </button>
       </>
     );
   }
